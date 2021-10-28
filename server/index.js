fa81d30cd35bdb4ce060718f25100f1ed622ff2f
briiking4 <brianaking626@gmail.com>
2021-10-28 14:21:52 -0700

    bug fixes

--- server/index.js
+++ server/index.js
@@ -15,7 +15,12 @@ const db = mysql.createPool({
 });
 
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
 
 var members;
 var events;
